853f627669ca2125c334bccb0368186cf7ab98e8
Jon Jarboe <jon.jarboe@contrastsecurity.com>
2020-04-14 18:23:02 -0700

    avoid localhost URLs due to docker; use service names instead

--- bookstore-frontend/src/main/java/acme/frontend/ServicePaths.java
+++ bookstore-frontend/src/main/java/acme/frontend/ServicePaths.java
@@ -7,6 +7,10 @@ final class ServicePaths {
 
   private ServicePaths() { }
 
-  static String DEBUG_URL = "http://localhost:8002/application/";
-  static String DATA_MANAGER_URL = "http://localhost:8001/";
+  //static String DEBUG_URL = "http://localhost:8002/application/";
+  static String DEBUG_URL = "http://bookstore-devservice:8002/application/";
+
+
+  // static String DATA_MANAGER_URL = "http://localhost:8001/";
+  static String DATA_MANAGER_URL = "http://bookstore-datamanager:8001/";
 }
